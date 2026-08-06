207cdd85f4beeb57695d91078e90c58169a7e839
Aadith <aadithaadhi33@gmail.com>
2026-08-06 09:54:48 +0530

    using function

--- Arithametic-Operations/tempratureconvertor.py
+++ Arithametic-Operations/tempratureconvertor.py
@@ -1,8 +1,21 @@
-var1=float(input("Enter the temprature "))
-choice=(input("Convert into Celcius:C Fahrenheit:F  "))
-if choice == "C":
-    print((var1 - 32) * 5/9)
-elif choice == "F":
-    print((var1 * 9/5) + 32)
-else:
-    print("Invalid Choice")
+def celsius_to_fahrenheit(var1):
+    return (var1 - 32) * 5/9;
+
+def fahrenheit_to_celsius(var1):
+    return ((var1 * 9/5) + 32);
+
+def main():
+
+    var1=float(input("Enter the temprature "))
+    choice=(input("Convert into Celcius:C Fahrenheit:F  "))
+
+    if choice == "C":
+        print(f"{celsius_to_fahrenheit(var1)}")
+
+    elif choice == "F":
+        print(f"{fahrenheit_to_celsius(var1)}")
+
+    else:
+        print("Invalid Choice")
+
+main()
