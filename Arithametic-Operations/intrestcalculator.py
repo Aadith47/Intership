3e1aff082bde0dba17618a5d996a2423f9b0325a
Aadith <aadithaadhi33@gmail.com>
2026-08-06 10:29:06 +0530

    using funtion

--- Arithametic-Operations/intrestcalculator.py
+++ Arithametic-Operations/intrestcalculator.py
@@ -1,9 +1,13 @@
-def intrest_calc():
+def intrest_calc(P,R,T):
+    result = (P*R*T)/100
+    return result
+
+def main():
 
     P=int(input("Enter the Principal "))
     R=int(input("Enter the Rate "))
     T=int(input("Enter the time "))
 
-    print((P*R*T)/100)
+    print(f"{intrest_calc(P,R,T)}")
 
-intrest_calc()
+main()
