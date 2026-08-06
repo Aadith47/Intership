709bee778294d8ee55be99be4bc3d82bbf7882f3
Aadith <aadithaadhi33@gmail.com>
2026-08-06 14:13:46 +0530

    .

---
+++ Logical-Operations-Condtionals/1comparison_operators.py
@@ -0,0 +1,15 @@
+def main():
+
+    num1=int(input("Enter the first number:"))
+    num2=int(input("Enter the second number:"))
+
+    if num1 > num2:
+        print("First one is greater")
+
+    elif num1 < num2:
+        print("First one smaller")
+
+    elif num1 == num2:
+        print("Same number")
+
+main()
