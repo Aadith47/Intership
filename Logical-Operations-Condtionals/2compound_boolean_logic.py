c398ccdf0e2393e16cae629049db9005ae455646
Aadith <aadithaadhi33@gmail.com>
2026-08-07 19:47:15 +0530

    fixed the function call

--- Logical-Operations-Condtionals/2compound_boolean_logic.py
+++ Logical-Operations-Condtionals/2compound_boolean_logic.py
@@ -1,14 +1,15 @@
 def leap_year_calc(year):
-    result=(year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
-    return result
-
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
+    
 def main():
-    year = int(input("Enter the year: "))
 
-    if leap_year_calc(year):
-     print("Leap year")
+    year = int(input("Enter the year: "))
+    leap_year = leap_year_calc(year)
+
+    if leap_year:
+        print(f"{year} is leap year")
 
     else:
-     print("Not leap year")
+        print(f"{year} is not a leap year")
 
 main()
