47eaba4a6027ffce94e64fd04d85614340aaf62b
Aadith <aadithaadhi33@gmail.com>
2026-08-07 19:48:30 +0530

    Fixed the function call

--- Logical-Operations-Condtionals/5truthiness_and_short_circuiting .py
+++ Logical-Operations-Condtionals/5truthiness_and_short_circuiting .py
@@ -1,26 +1,27 @@
-def lenth_in_pass(password):
-   result1 = len(password) < 8
-   return result1
+def length_in_pass(password):
+   return len(password) < 8
 
 def uppercase_in_pass(password):
-   result2 = any(char.isupper() for char in password)
-   return result2
+   return any(char.isupper() for char in password)
 
 def digit_in_pass(password):
-   result3 = any(char.isdigit() for char in password)
-   return result3
+   return any(char.isdigit() for char in password)
 
 def main():
 
     password=input("Enter the passsword:")
 
-    if lenth_in_pass(password):
+    result1_in_pass = length_in_pass(password)
+    result2_in_pass = uppercase_in_pass(password)
+    result3_in_pass = digit_in_pass(password)
+
+    if result1_in_pass:
         print("MUST CONTAIN 8 LONG")
 
-    elif not uppercase_in_pass(password):
+    elif not result2_in_pass:
         print("MUST CONTAIN AN UPPERCASE LETTER")
 
-    elif not digit_in_pass(password):
+    elif not result3_in_pass:
         print("MUST CONTAIN A DIGIT")
 
     else:
