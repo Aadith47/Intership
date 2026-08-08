92d5d5b975a6cf6e825bc38ef3c2ccd4e824d8de
Aadith <aadithaadhi33@gmail.com>
2026-08-08 11:28:13 +0530

    updated

--- Logical-Operations-Condtionals/6ternary_expressions.py
+++ Logical-Operations-Condtionals/6ternary_expressions.py
@@ -1,15 +1,3 @@
-def even(num):
-    return num % 2 == 0
 
-def main():
-
-    num=float(input("Enter the number:"))
-    result=even(num)
-
-    if result:
-        print("Even")
- 
-    else:
-        print("odd")
-
-main()
+num=int(input("Enter a number:"))
+print(["even", 'odd'][num %2])
