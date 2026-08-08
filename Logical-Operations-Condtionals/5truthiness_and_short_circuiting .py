929a9a80bfe95bb6fea81a43a9994cf12e48e6f0
Aadith <aadithaadhi33@gmail.com>
2026-08-08 10:51:00 +0530

    fixed the condtions error

--- Logical-Operations-Condtionals/5truthiness_and_short_circuiting .py
+++ Logical-Operations-Condtionals/5truthiness_and_short_circuiting .py
@@ -1,31 +1,45 @@
 def length_in_pass(password):
-   return len(password) < 8
+   if len(password) < 8:
+      return "MUST CONTAIN 8 CHARACTERS"
+   else:
+      return ""
 
 def uppercase_in_pass(password):
-   return any(char.isupper() for char in password)
+   if not any(char.isupper() for char in password):
+      return "MUST CONTAIN AN UPPERCASE LETTER"
+   else:
+      return ""
 
 def digit_in_pass(password):
-   return any(char.isdigit() for char in password)
+   if not any(char.isdigit() for char in password):
+      return "MUST CONTAIN A DIGIT"
+   else:
+      return ""
 
 def main():
 
     password=input("Enter the passsword:")
 
-    result1_in_pass = length_in_pass(password)
-    result2_in_pass = uppercase_in_pass(password)
-    result3_in_pass = digit_in_pass(password)
+    result1 = length_in_pass(password)
+    result2 = uppercase_in_pass(password)
+    result3 = digit_in_pass(password)
 
-    if result1_in_pass:
-        print("MUST CONTAIN 8 LONG")
 
-    elif not result2_in_pass:
-        print("MUST CONTAIN AN UPPERCASE LETTER")
-
-    elif not result3_in_pass:
-        print("MUST CONTAIN A DIGIT")
+    if result1 == "" and result2 == "" and result3 == "":
+        print("Strong password!")
 
     else:
-        print("STRONG PASSWORD")
+        print("Password:")
+
+        if result1:
+            print(result1)
+
+        if result2:
+            print(result2)
+
+        if result3:
+            print(result3)
+      
 
 main()
     
