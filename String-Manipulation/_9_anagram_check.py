345e8bcb0f057ff99e8dbd763bea08d7f746592c
Aadith <aadithaadhi33@gmail.com>
2026-08-11 19:51:49 +0530

    anagram

---
+++ String-Manipulation/_9_anagram_check.py
@@ -0,0 +1,18 @@
+def anagram_checker(string1, string2):
+    letters1=sorted(string1)
+    letters2=sorted(string2)
+
+    if letters1==letters2:
+        return "anagram"
+    
+    else:
+        return "Not anagram"
+
+def main():
+
+    string1=input("Enter the first string:")
+    string2=input("Enter the second string:") 
+    result1=anagram_checker(string1, string2)
+    print(result1)
+
+main()
