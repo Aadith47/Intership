e5874dca083dbf915729f1595af4d051f5b9051b
Aadith <aadithaadhi33@gmail.com>
2026-08-11 10:22:27 +0530

    updated using function

--- Loops-Iteration/_1_state_tracking_across_iterations.py
+++ Loops-Iteration/_1_state_tracking_across_iterations.py
@@ -1,7 +1,15 @@
-previous = 0
-running_sum = 0
+def state_tracking():
 
-for current in range(0, 10):
-    running_sum += current
-    print(f"Current {current} Previous {previous} Sum {running_sum}")
-    previous = current
+    previous = 0
+    running_sum = 0
+
+    for current in range(0, 10):
+        running_sum += current
+        print(f"Current {current} Previous {previous} Sum {running_sum}")
+        previous = current
+
+def main():
+
+    state_tracking()
+
+main()
