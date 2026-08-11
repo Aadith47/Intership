4b88afb914dd0b966698c038d99062d41fcf486a
Aadith <aadithaadhi33@gmail.com>
2026-08-11 10:23:48 +0530

    ...

--- Loops-Iteration/_5_dynamic_exit_condition.py
+++ Loops-Iteration/_5_dynamic_exit_condition.py
@@ -1,15 +1,21 @@
 # Starting from a positive integer, repeatedly apply: if even, divide by 2; if odd, multiply by 3 and add 1. Count the steps until the value reaches 1. 
+def step_counter(num):
+    count = 0
 
-num = int(input("Enter a starting number: "))
-count = 0
+    while num != 1:
 
-while num != 1:
+        if num % 2 == 0:
+            num = num // 2
 
-    if num % 2 == 0:
-        num = num // 2
+        else:
+            num = num * 3 + 1
+        count += 1
 
-    else:
-        num = num * 3 + 1
-    count += 1
+    print(f"Steps: {count}")
 
-print(f"Steps: {count}")
+def main():
+
+    num = int(input("Enter a starting number:"))
+    step_counter(num)
+
+main()
