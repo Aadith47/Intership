7aaf0f4637bfc33a1b3583509334352a352898a7
Aadith <aadithaadhi33@gmail.com>
2026-08-16 23:16:12 +0530

    ...

---
+++ Hashmaps-Dictionaries/_1_key_value_consolidation.py
@@ -0,0 +1,20 @@
+student = {
+
+    "name":"aadith",
+    "age" : 22,
+    "course" : "python"
+
+}
+
+course = {
+
+    "company Name" : "BlueFox",
+    "place" : "ernakulam",
+    "period" : "6months"
+
+}
+
+student.update(course)
+
+print(student)
+
