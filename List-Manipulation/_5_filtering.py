ad4531219007dadd79cd34f955eeca21398ef7be
Aadith <aadithaadhi33@gmail.com>
2026-08-17 11:04:37 +0530

    ...

--- List-Manipulation/_5_filtering.py
+++ List-Manipulation/_5_filtering.py
@@ -1,9 +1,29 @@
-def main():
-
-    list1=[20,22,44,3,12,40,27,60]
+list1=[20,22,44,3,12,40,27,60]
+def func():
 
     for i in (list1):
         if i % 5 == 0:
             print(i)
 
+
+def main():
+
+    func()
+
 main()
+
+
+
+
+
+
+
+# def main():
+
+#     list1=[20,22,44,3,12,40,27,60]
+
+#     for i in (list1):
+#         if i % 5 == 0:
+#             print(i)
+
+# main()
