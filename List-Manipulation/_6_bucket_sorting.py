335cf8439f05acf921f85b5f32cf0dd52a858f79
Aadith <aadithaadhi33@gmail.com>
2026-08-17 11:26:13 +0530

    ...

--- List-Manipulation/_6_bucket_sorting.py
+++ List-Manipulation/_6_bucket_sorting.py
@@ -1,15 +1,37 @@
-def main():
+list1=[1,2,3,4,5,6,7,8,9,10]
+list_even=[]
+list_odd=[]
 
-    list1=[1,2,3,4,5,6,7,8,9,10]
-    list_even=[]
-    list_odd=[]
-
+def func():
     for i in (list1):
         if i % 2 == 0:
             list_even.append(i)
         else:
             list_odd.append(i)
 
+def main():
+    
+    func()
     print(f"Even List = {list_even}\nOdd List = {list_odd}")
 
 main()
+
+
+
+
+
+# def main():
+
+#     list1=[1,2,3,4,5,6,7,8,9,10]
+#     list_even=[]
+#     list_odd=[]
+
+#     for i in (list1):
+#         if i % 2 == 0:
+#             list_even.append(i)
+#         else:
+#             list_odd.append(i)
+
+#     print(f"Even List = {list_even}\nOdd List = {list_odd}")
+
+# main()
