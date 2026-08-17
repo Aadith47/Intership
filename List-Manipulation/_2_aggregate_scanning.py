1a5c22310a38392892bbb0403900cdc39eeb2f56
Aadith <aadithaadhi33@gmail.com>
2026-08-17 11:02:14 +0530

    ...

--- List-Manipulation/_2_aggregate_scanning.py
+++ List-Manipulation/_2_aggregate_scanning.py
@@ -1,8 +1,8 @@
-def main():
-      
-    list_of_number=[45, 2, 89, 12, 7]
-    largest_num=list_of_number[0]
-    smallest_num=list_of_number[0]
+list_of_number=[45, 2, 89, 12, 7]
+largest_num=list_of_number[0]
+smallest_num=list_of_number[0]
+
+def numbers(largest_num,smallest_num):
 
     for i in (list_of_number):
             if i > largest_num:
@@ -11,7 +11,42 @@ def main():
             if i < smallest_num:
                 smallest_num = i
 
+def main():
+
+    numbers(largest_num,smallest_num)
     print(f"The largest number is {largest_num}\nThe smallest number is {smallest_num}")
 
 main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     def main():
+      
+#     list_of_number=[45, 2, 89, 12, 7]
+#     largest_num=list_of_number[0]
+#     smallest_num=list_of_number[0]
+
+#     for i in (list_of_number):
+#             if i > largest_num:
+#                 largest_num = i
+
+#             if i < smallest_num:
+#                 smallest_num = i
+
+#     print(f"The largest number is {largest_num}\nThe smallest number is {smallest_num}")
+
+# main()
     
