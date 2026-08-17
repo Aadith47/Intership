c357b5911e27ee2262335f740de100079b51c409
Aadith <aadithaadhi33@gmail.com>
2026-08-17 11:27:04 +0530

    ...

--- List-Manipulation/_9_slicing_and_reassembly.py
+++ List-Manipulation/_9_slicing_and_reassembly.py
@@ -1,8 +1,13 @@
+list1=[1,2,3,4,5]
+def func(n):
+    
+    list2 = list1[n:] + list1[:n]
+    return list2
+
 def main():
 
-    list1=[1,2,3,4,5]
     n=int(input("Enter the number of rotation: "))
-    list2 = list1[n:] + list1[:n]
-    print(list2)
+    result1=func(n)
+    print(result1)
 
 main()
