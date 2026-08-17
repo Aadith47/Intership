8af5daecac95b18db6bec039c58f1db4977bc698
Aadith <aadithaadhi33@gmail.com>
2026-08-17 11:26:49 +0530

    ...

--- List-Manipulation/_8_nested_structures.py
+++ List-Manipulation/_8_nested_structures.py
@@ -1,2 +1,16 @@
-list1=[1,[2,3],[4,[5,6]]]
+list1 = [1, [2, 3], [4, [5, 6]]]
+def flatten(lst):
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
 
+def main():
+
+    result1=flatten(list1)
+    print(result1)
+
+main()
